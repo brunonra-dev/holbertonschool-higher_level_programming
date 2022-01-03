dbfa390e7a924a4910e94d4125c68b4b0a7c573f
Bruno Rodriguez <40237383+brunonra-dev@users.noreply.github.com>
2022-01-03 20:36:53 +0000

    fix

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
+    x = 0
     if idx < 0:
         return my_list
     if idx > len(my_list) - 1:
         return my_list
     for i in my_list:
-        if i == idx:
+        if i == x:
             my_list[i] = element
             return my_list
+        x += 1
