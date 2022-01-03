c3b67dff45bae4799771a93167eda9a9898a66e4
Bruno Rodriguez <40237383+brunonra-dev@users.noreply.github.com>
2022-01-03 20:52:02 +0000

    added 4-new_in_list.py

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    cpy = my_list.copy()
+    if idx < 0:
+        return cpy
+    if idx > len(my_list) - 1:
+        return cpy
+    cpy[idx] = element
+    return cpy
