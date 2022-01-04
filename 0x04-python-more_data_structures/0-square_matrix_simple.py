319ff8924af4602977ced12d2d722a3eed5647ca
Bruno Rodriguez <40237383+brunonra-dev@users.noreply.github.com>
2022-01-04 22:41:11 +0000

    fix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    newm = [list(map(lambda x: x ** 2, i)) for i in matrix] 
+    newm = [list(map(lambda x: x ** 2, i)) for i in matrix]
     return newm
