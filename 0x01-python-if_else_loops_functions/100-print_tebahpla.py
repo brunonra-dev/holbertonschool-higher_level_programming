813f393a2f3de952ff8cabd0e27b928cf3cedc2c
Bruno Rodriguez <40237383+brunonra-dev@users.noreply.github.com>
2022-01-10 14:09:55 +0000

    fix

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 x = 0
 for i in reversed(range(97, 123)):
-    print('{:c}'.format(i - n), end='')
+    print('{:c}'.format(i - x), end='')
     if x == 0:
         x = 32
     else:
