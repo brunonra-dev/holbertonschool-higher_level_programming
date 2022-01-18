09192d829f42453ab178be1383f8831fd85eadef
Bruno Rodriguez <40237383+brunonra-dev@users.noreply.github.com>
2022-01-18 14:57:51 -0800

    4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""Square module
+Task of 0x06. Python - Classes and Objects
+
+"""
+
+
+class Square:
+    """Description of Square Class
+
+    Attributes:
+        __size (int): size of square
+
+    Returns:
+        area: return area of Square
+
+    """
+
+    def __init__(self, size=0):
+        """
+        size (int): size of square
+
+        Args:
+            size: size of square
+
+        Raises:
+            TypeError: If size is not int.
+            ValueError: If size < 0.
+
+        """
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def size(self):
+        """return size of Square"""
+
+        return(self.__size)
+
+    def size(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        """return area of Square"""
+
+        return (self.__size**2)
