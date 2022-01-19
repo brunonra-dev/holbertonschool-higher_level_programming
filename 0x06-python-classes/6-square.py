e1ff0c304a677f6f867de59ba13f1580f182688b
Bruno Rodriguez <40237383+brunonra-dev@users.noreply.github.com>
2022-01-18 16:46:32 -0800

    Revert "fix"
    This reverts commit 8ec86a34729493b93120c995d019aa0c73166af2.

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -36,7 +36,8 @@ class Square:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-
+        if (not isinstance(position, type((int, int)))):
+            raise TypeError("position must be a tuple of 2 positive integers")
         if (position[0] < 0 or position[1] < 0):
             raise TypeError("position must be a tuple of 2 positive integers")
 
@@ -70,11 +71,9 @@ class Square:
     def position(self, value):
         """sets position of square"""
 
-        if type(value) is not tuple or len(value) is not 2:
+        if (not isinstance(position, type((int, int)))):
             raise TypeError("position must be a tuple of 2 positive integers")
-        if type(value[0]) is not int or type(value[1]) is not int:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if value[0] < 0 or value[1] < 0:
+        if (value[0] < 0 or value[1] < 0):
             raise TypeError("position must be a tuple of 2 positive integers")
 
         self.__position = value
