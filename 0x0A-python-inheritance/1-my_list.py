c8e6e7b1f7165980b14035bb6c8f8afcb47c2495
Bruno Rodriguez <40237383+brunonra-dev@users.noreply.github.com>
2022-01-31 15:39:51 -0800

    added 1-my_list.py

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+MyList classes
+"""
+
+class MyList(list):
+    """
+    prints the list, but sorted (ascending sort)
+    """
+    def print_sorted(self):
+        print(sorted(self))
