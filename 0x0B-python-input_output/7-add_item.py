833ed5968e041148af2ae7bd605098265fa7f405
Bruno Rodriguez <40237383+brunonra-dev@users.noreply.github.com>
2022-02-01 14:40:21 -0800

    added 7-add_item.py

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+load_add_save module
+"""
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+if open('add_item.json'):
+    objs = load_from_json_file('add_item.json')
+else:
+    objs = []
+
+for n in range(1, len(sys.argv)):
+    objs.append(sys.argv[n])
+
+save_to_json_file(objs, 'add_item.json')
