1e00bcafcd3631b0ddb9812901c2035eff453e02
Bruno Rodriguez <40237383+brunonra-dev@users.noreply.github.com>
2022-02-01 14:59:44 -0800

    fix

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -12,9 +12,9 @@ class Student:
         - age
     """
     def __init__(self, first_name, last_name, age):
-        first_name = first_name
-        last_name = last_name
-        age = age
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
     def to_json(self):
         return self.__dict__
