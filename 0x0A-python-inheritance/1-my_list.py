589371b834a7214aafbd562e94428cf1866a6a6d
Bruno Rodriguez <40237383+brunonra-dev@users.noreply.github.com>
2022-01-31 16:05:38 -0800

    fix

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -8,4 +8,8 @@ class MyList(list):
     prints the list, but sorted (ascending sort)
     """
     def print_sorted(self):
+        for item in self:
+            if type(item) != int:
+                raise TypeError
+
         print(sorted(self))
