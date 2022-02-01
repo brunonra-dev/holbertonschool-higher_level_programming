5474d3b7021e26ec642bb33452b943958833a92f
Bruno Rodriguez <40237383+brunonra-dev@users.noreply.github.com>
2022-01-31 16:26:02 -0800

    added 6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+BaseGeometry class
+"""
+
+
+class BaseGeometry:
+    """
+    Raises:
+        - Exception: "area() is not implemented"
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
