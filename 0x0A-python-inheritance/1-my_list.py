647502581f4a35d5a0855626f9ae18c33a06018a
Bruno Rodriguez <40237383+brunonra-dev@users.noreply.github.com>
2022-01-31 16:09:44 -0800

    fix

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -8,8 +8,4 @@ class MyList(list):
     prints the list, but sorted (ascending sort)
     """
     def print_sorted(self):
-        for item in self:
-            if type(item) != int:
-                raise TypeError("must be a list of integers")
-
         print(sorted(self))
