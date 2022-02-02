b3600ae60830733d750594d8ac7e08e1e7ae2881
Bruno Rodriguez <40237383+brunonra-dev@users.noreply.github.com>
2022-02-02 09:54:49 -0800

    added 12-pascal_triangle.py

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+pascal_triangle module
+"""
+
+
+def pascal_triangle(n):
+    """
+    returns a list of lists of integers representing the Pascal’s triangle
+    """
+    tri = []
+    tmp = [1]
+    if n == 0:
+        return tri
+
+    for i in range(n):
+        tmp = [1]
+        for j in range(i):
+            tmp.append(i)
+        tmp[-1] = 1
+        tri.append(tmp)
+    
+    return tri
