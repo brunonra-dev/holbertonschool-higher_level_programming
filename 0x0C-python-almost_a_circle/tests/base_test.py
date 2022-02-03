8064714d850cb41a2e4bdab39b7f1634865b7d83
Bruno Rodriguez <40237383+brunonra-dev@users.noreply.github.com>
2022-02-03 08:02:49 -0800

    Update

--- 0x0C-python-almost_a_circle/tests/base_test.py
+++ 0x0C-python-almost_a_circle/tests/base_test.py
@@ -35,4 +35,37 @@ class test_base(unittest.TestCase):
         b1 = Base()
         b2 = Base()
         self.assertEqual(print(b2.id), 2)
-        
+
+    def base_id_inc2(self):
+        """
+        check if base id increment
+        """
+        b1 = Base()
+        b2 = Base(12)
+        b3 = Base()
+        self.assertEqual(print(b2.id), 2)
+
+    def base_id_neg(self):
+        """
+        check base id negaative
+        """
+        b1 = Base(-1)
+        self.assertEqual(print(b1.id), -1)
+
+    def base_id_str(self):
+        """
+        check base id str
+        """
+        self.assertRaises(TypeError, Base, "hola")
+
+    def base_id_float(self):
+        """
+        check base id float
+        """
+        self.assertRaises(TypeError, Base, 1.0)
+
+    def base_id_bool(self):
+        """
+        check base id bool
+        """
+        self.assertRaises(TypeError, Base, True)
