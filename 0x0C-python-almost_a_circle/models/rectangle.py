bb88ce13697bda6ab0881792b4472c1782e32e3f
Bruno <bruno199723@gmail.com>
2022-02-07 19:51:19 -0300

    added comments

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -7,6 +7,7 @@ from models.base import Base
 
 
 class Rectangle(Base):
+    """the class Rectangle that inherits from Base"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
         if type(width) is not int:
