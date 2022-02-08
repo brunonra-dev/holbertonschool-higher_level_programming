7e96696f5fff33e96b44776b497f90b10a01011b
Bruno <bruno199723@gmail.com>
2022-02-08 00:06:03 -0300

    added more tests

--- 0x0C-python-almost_a_circle/tests/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -13,19 +13,19 @@ class test_rectangle(unittest.TestCase):
 
     def test_make_rectangle(self):
         """
-        check if Rectangle no args
+        check Rectangle no args
         """
         self.assertRaises(TypeError, Rectangle)
 
     def test_make_rectangle_12(self):
         """
-        check if Rectangle with 1 arg
+        check Rectangle with 1 arg
         """
         self.assertRaises(TypeError, Rectangle, 5)
 
     def test_make_rectangle_args(self):
         """
-        check if Rectangle with 2 agrs
+        check Rectangle with 2 agrs
         """
         self.assertEqual(Rectangle(4, 5).id, 1)
 
@@ -35,7 +35,13 @@ class test_rectangle(unittest.TestCase):
         """
         b1 = Rectangle(4, 5)
         b2 = Rectangle(2, 4)
-        self.assertEqual(b2.id, 3)
+        self.assertEqual(b2.id, 4)
+
+    def test_make_rectangle_args2(self):
+        """
+        check Rectangle with 3 agrs
+        """
+        self.assertEqual(Rectangle(4, 5, 1).id, 2)
 
     def test_rectangle_id_inc2(self):
         """
@@ -48,11 +54,27 @@ class test_rectangle(unittest.TestCase):
 
     def test_rectangle_id_neg(self):
         """
-        check Rectangle id negaative
+        check Rectangle id negative
         """
         self.assertEqual(Rectangle(2, 4, 0, 0, -1).id, -1)
 
+    def test_str(self):
+        """
+        check Rectangle with str
+        """
+        self.assertRaises(TypeError, Rectangle, "hola")
+
+    def test_neg(self):
+        """
+        check Rectangle with negative
+        """
+        self.assertRaises(ValueError, Rectangle, -1, -1)
     
+    def test_neg_pos(self):
+        """
+        check Rectangle with negative position
+        """
+        self.assertRaises(ValueError, Rectangle, 10, 10, -1, -1)
 
 if __name__ == '__main__':
     unittest.main()
