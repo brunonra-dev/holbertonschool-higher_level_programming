4b918c941645d16885f194fb2a2011bf04e97d22
brunonra-dev <40237383+brunonra-dev@users.noreply.github.com>
2022-04-05 20:05:01 -0300

    fix

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -4,6 +4,7 @@ const Rectangle = require('./4-rectangle');
 module.exports = class Square extends Rectangle {
   constructor (size) {
     super(size, size);
+    this.size = size;
   }
 
   charPrint (c) {
