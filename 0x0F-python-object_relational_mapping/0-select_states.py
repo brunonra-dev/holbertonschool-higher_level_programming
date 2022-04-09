0370bae3006507f1978e65e99c6d69097282b34e
brunonra-dev <40237383+brunonra-dev@users.noreply.github.com>
2022-04-09 12:36:02 -0300

    Task 0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""lists all states from the database hbtn_0e_0_usa"""
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3], charset="utf8")
+
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+    cursor.close()
+    conn.close()
