773040780bce30e37205744072b1e403ac4b5c70
Bruno Rodríguez <40237383+brunonra-dev@users.noreply.github.com>
2022-05-17 14:22:40 -0300

    task 1

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/*
+writes a string to a file
+*/
+
+const fs = require('fs');
+
+fs.writeFile(process.argv[2], process.argv[3], 'utf-8', (err) => {
+  if (err) {
+    console.log(err);
+  }
+});
