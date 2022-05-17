4741998fbd6742c07fda701c633ba06f5a030a4d
Bruno Rodríguez <40237383+brunonra-dev@users.noreply.github.com>
2022-05-17 15:11:57 -0300

    task 4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+/*
+Prints the title of a Star Wars movie where
+the episode number matches a given integer
+*/
+
+const axios = require('axios').default;
+const character = 'https://swapi-api.hbtn.io/api/people/18/';
+
+axios.get(process.argv[2])
+  .then(function (response) {
+    let count = 0;
+    for (let movie of response.data.results) {
+      if (response.data.results[movie].characters.includes(character)) {
+        num = num + 1;
+      }
+    }
+    console.log(count);
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
